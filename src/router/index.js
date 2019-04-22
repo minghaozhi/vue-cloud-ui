6b82b314a37241110807ec09fde97fd68506496b
minghaozhi <597575122@qq.com>
2019-04-22 18:33:56 +0800

    vue基础页面

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
 import Login from '@/views/login.vue'
-
+import home from '@/views/home.vue'
+import error from '@/views/404.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -11,6 +12,16 @@ export default new Router({
       path: '/',
       name: '登录',
       component: Login
+    },
+    {
+      path: '/home',
+      name: '首页',
+      component: home
+    },
+    {
+      path: '/error',
+      name: '找不到页面',
+      component: error
     }
   ]
 })
---
+++ src/views/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h2>home Page</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home'
+  }
+</script>
+
+<style scoped>
+
+</style>
